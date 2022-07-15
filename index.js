f17330ad624ebc1d6e76bff60fb070bb8acbc56d
Fabian Steuer <faebse@posteo.de>
2022-07-15 12:06:42 +0100

    Refactor into modules

--- index.js
+++ index.js
@@ -1,40 +1,31 @@
-require("dotenv").config();
-const cors = require("cors");
 const express = require("express");
 const mongoose = require("mongoose");
+const cors = require("cors");
+const config = require("./utils/config");
+const logger = require("./utils/logger");
+const middleware = require("./utils/middleware");
+const blogsRouter = require("./controllers/blogs.js");
 
 const app = express();
 
-const blogSchema = new mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number,
-});
-
-const Blog = mongoose.model("Blog", blogSchema);
-
-const mongoUrl = process.env.MONGODB_URL;
-mongoose.connect(mongoUrl);
+mongoose
+  .connect(config.MONGODB_URI)
+  .then(() => {
+    logger.info("Connected to MongoDB");
+  })
+  .catch((error) => {
+    logger.error("Error connecting to MongoDB:", error.message);
+  });
 
 app.use(cors());
 app.use(express.json());
+app.use(middleware.requestLogger);
 
-app.get("/api/blogs", (request, response) => {
-  Blog.find({}).then((blogs) => {
-    response.json(blogs);
-  });
-});
+app.use("/api/blogs", blogsRouter);
 
-app.post("/api/blogs", (request, response) => {
-  const blog = new Blog(request.body);
+app.use(middleware.unknownEndpoint);
+app.use(middleware.errorHandler);
 
-  blog.save().then((result) => {
-    response.status(201).json(result);
-  });
-});
-
-const PORT = process.env.PORT;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+app.listen(config.PORT, () => {
+  logger.info(`Server running on port ${config.PORT}`);
 });
