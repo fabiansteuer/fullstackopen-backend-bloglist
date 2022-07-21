60fbb2861fb278cfd536e1e5357a5789ba73aa39
Fabian Steuer <faebse@posteo.de>
2022-07-21 19:23:00 +0100

    Link created blogs to random user

--- controllers/users.js
+++ controllers/users.js
@@ -4,7 +4,12 @@ const User = require("../models/user");
 const bcrypt = require("bcrypt");
 
 usersRouter.get("/", async (request, response) => {
-  const users = await User.find({});
+  const users = await User.find({}).populate("blogs", {
+    title: 1,
+    author: 1,
+    url: 1,
+    likes: 1,
+  });
   return response.json(users);
 });
 
